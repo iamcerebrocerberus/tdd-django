749d8b7e341f7d6f2b2658578970ff80c39cae51
iamcerebrocerberus <sethamekmen@gmail.com>
2024-06-08 17:02:32 -0400

    feat: adjust model so that items are associated with different lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -20,15 +20,23 @@ class NewListTest(TestCase):
         self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        mylist = List()
+        mylist.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = mylist
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = mylist
         second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, mylist)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -36,7 +44,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, mylist)
         self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, mylist)
 
 
 class ListViewTest(TestCase):
@@ -44,8 +54,9 @@ class ListViewTest(TestCase):
         response = self.client.get("/lists/the-only-list-in-the-world/")
         self.assertTemplateUsed(response, "list.html")
     def test_display_all_list_items(self):
-        Item.objects.create(text="item 1")
-        Item.objects.create(text="item 2")
+        mylist = List.objects.create()
+        Item.objects.create(text="item 1", list=mylist)
+        Item.objects.create(text="item 2", list=mylist)
         response = self.client.get("/lists/the-only-list-in-the-world/")
         self.assertContains(response, "item 1")
         self.assertContains(response, "item 2")
